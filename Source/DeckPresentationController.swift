480271e5073694c5bca7bb92970a090d9e42f910
Rodrigo Kreutz <rodrigo.kreutz@teamwork.com>
2018-10-01 15:21:03 +0100

    Updating Swift version to 4.2

--- Source/DeckPresentationController.swift
+++ Source/DeckPresentationController.swift
@@ -67,7 +67,7 @@ final class DeckPresentationController: UIPresentationController, UIGestureRecog
         self.dismissAnimation = dismissAnimation
         self.dismissCompletion = dismissCompletion
         
-        NotificationCenter.default.addObserver(self, selector: #selector(updateForStatusBar), name: .UIApplicationDidChangeStatusBarFrame, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateForStatusBar), name: UIApplication.didChangeStatusBarFrameNotification, object: nil)
     }
 
     // MARK: - Public methods
@@ -349,7 +349,7 @@ final class DeckPresentationController: UIPresentationController, UIGestureRecog
         }
         
         updateSnapshotViewAspectRatio()
-        containerView.bringSubview(toFront: roundedViewForPresentedView)
+        containerView.bringSubviewToFront(roundedViewForPresentedView)
         
         if presentedViewController.view.isDescendant(of: containerView) {
             UIView.animate(withDuration: 0.1) { [weak self] in
